012b44ac1fde2a0bffa17dcca82c7bb4819eb41c
KNWking <knwking@163.com>
2024-05-28 17:50:31 +0800

    Create H.cpp

---
+++ [CUMT] 数据结构作业/Contest1097 - 数据结构作业1-唐朝刚老师/H.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+stack<char> s;
+int n;
+char c;
+
+// 同 hdu 1062.
+
+int main(){
+    scanf("%d", &n);
+    getchar();  // 捕捉第一行数字后的 '\n'.
+    while(scanf("%c", &c) != EOF){
+        if(c == '\n' || c == ' '){
+            while(!s.empty()){  // 清空栈。
+                printf("%c", s.top());
+                s.pop();
+            }
+            putchar(c);
+        }else s.push(c);
+    }
+    while(!s.empty()){
+        // 增加鲁棒性，当最后一行文本没有换行就 EOF 了依然能 AC。
+        printf("%c", s.top());
+        s.pop();
+    }
+    printf("\n");
+    return 0;
+}

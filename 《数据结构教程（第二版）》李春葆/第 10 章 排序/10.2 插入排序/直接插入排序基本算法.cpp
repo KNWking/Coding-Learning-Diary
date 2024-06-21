d5530f99c6dcdee1ff0053dd342e5c4af95cf7e1
KNWking <knwking@163.com>
2024-06-21 15:32:25 +0800

    Create 直接插入排序基本算法.cpp

---
+++ 《数据结构教程（第二版）》李春葆/第 10 章 排序/10.2 插入排序/直接插入排序基本算法.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+vector<int> arr{9, 8, 7, 6, 5, 4, 3, 2, 1, 0};
+
+void strightSort() {
+    for (int i = 1; i < arr.size(); ++i) {
+        int key = arr[i];
+        int j = i - 1;
+        while (j >= 0 && arr[j] > key) {
+            arr[j + 1] = arr[j];
+            j--;
+        }
+        arr[j + 1] = key;
+    }
+
+}
+
+int main() {
+    strightSort();
+    for (auto a: arr)
+        cout << a << " ";
+    return 0;
+}

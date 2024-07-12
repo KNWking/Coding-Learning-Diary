20fa7c196b72610f5bbacd73e85178b7d675b66f
KNWking <knwking@163.com>
2024-07-12 15:07:49 +0800

    Update poj 1521.cpp

--- 《算法竞赛》/1 基础数据结构/1.4 二叉树和哈夫曼树/poj 1521.cpp
+++ 《算法竞赛》/1 基础数据结构/1.4 二叉树和哈夫曼树/poj 1521.cpp
@@ -1,29 +1,42 @@
-#include <bits/stdc++.h>
+#include <cstdio>
+#include <iostream>
+#include <algorithm>
+#include <queue>
 using namespace std;
 
-int main(){
-	priority_queue <int, vector<int>, greater<int>> pq;
-	// 新建一个最小堆。
-	string s;
-	while(getline(cin, s) && s != "END"){
-		sort(s.begin(), s.end());
-		int num = 1;      // 一种字符出现的次数。
-	    for(int i = 1; i <= s.length(); i++){
-			if(s[i] != s[i-1]){ q.push(num);  num = 1;}
-	        else num++;
-		}
-		int ans = 0;
-        if(q.size() == 1)  // 题目的一个坑：只有一种字符的情况。
-        ans = s.length();
-		while(q.size() > 1){  //最后一次合并不用加到 ans 中。		   
-			int a = q.top(); q.pop();  //贪心：取出频次最少的两个。     
-            int b = q.top(); q.pop();
-            q.push(a+b);  //把两个最小的合并成新的结点，重新放进队列。
-            ans += a+b;  //一种字符进几次队列，就累加几次。
-            //进一次队列，表示它在二叉树上深了一层，编码长度加 1。
-		}
-		q.pop();
-		printf("%d %d %.1f\n", s.length() * 8, ans, (double) s.length() * 8 / (double) ans);
-	}
-	return 0;
+priority_queue<int, vector<int>, greater<int>> pq;
+string s;
+
+int main() {
+    while (getline(cin, s) && s != "END") {
+        sort(s.begin(), s.end());
+        int num = 1;
+        for (int i = 1; i < s.size(); ++i) {
+            if (s[i] != s[i - 1]) {
+                pq.push(num);
+                num = 1;  // 一种字符出现的次数。
+            } else {
+                num++;
+            }
+        }
+        pq.push(num); // 确保将最后一个字符的频率推入队列。
+        // 与原书中不同，防止数组越界。
+        int ans = 0;  // huffman 编码长度。
+        if (pq.size() == 1) {  // 题目的一个坑：只有一种字符的情况。
+            ans = s.size();
+        } else {
+            while (pq.size() > 1) {  // 最后一次合并不用加到 ans 中。
+                int a = pq.top();  // 贪心：取出频次最少的两个。
+                pq.pop();
+                int b = pq.top();
+                pq.pop();
+                pq.push(a + b);
+                ans += a + b;  // 一种字符进几次队列，就累加几次。
+                // 进一次队列，表示它在二叉树上深了一层，编码长度加 1。
+            }
+        }
+        pq.pop(); // 清空最后一个元素。
+        printf("%d %d %.1f\n", s.size() * 8, ans, (double) s.size() * 8 / (double) ans);
+    }
+    return 0;
 }
